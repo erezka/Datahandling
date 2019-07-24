5ec17993fb59daee447aec2279fada7bdedc3575
shevchuk <soretatosha@gmail.com>
2019-07-24 17:53:06 +0400

    chernovik

--- src/Task1/Task1_3.java
+++ src/Task1/Task1_3.java
@@ -1,10 +1,21 @@
 package Task1;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 public class Task1_3 {
     public static void main(String[] args) {
-        String date = "Friday, Aug 10, 2016 12:10:56 PM";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYY");
+        String date = "Friday, Aug 12, 2016 12:10:56 PM";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("cccc, MMM dd, yyyy hh:mm:ss");
+        LocalDateTime date1 = LocalDateTime.now();
+
+        String text = date1.format(formatter);
+        //LocalDateTime x = LocalDateTime.parse(date, formatter);
+
+        System.out.println(text);
     }
 }
+https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
